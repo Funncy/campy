12c5a90d6995572d705fb0c04de018bdb44d4aa9
funncy <gywns41@naver.com>
2019-05-31 00:32:07 +0900

    join.html 최초로그인 시 진입 기능

--- app_account_management/views.py
+++ app_account_management/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login as auth_login
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+from .models import StudentInfo
+from allauth.socialaccount.models import SocialAccount
 
 # Create your views here.
+
+@login_required
+def join(request):
+    print(request.user.id)
+
+    if request.method == 'GET':
+        #StudentInfo가 없을시 최초 로그인
+        try:
+            student_info = StudentInfo.objects.get(user_id=request.user.id)
+        except StudentInfo.DoesNotExist:
+            return render(request, 'join.html', {})
+
+        #Studentinfo가 있을경우 메인화면으로 이동
+        return redirect(reverse('index'))
+    print('POST')
+    return
