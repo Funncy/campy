d83fe4a784e29972a51d376c67e1f5e834f24b7d
funncy <gywns41@naver.com>
2019-06-13 18:58:05 +0900

    join url 수정

--- app_account_management/urls.py
+++ app_account_management/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('join/', views.join, name='join'),
+    path('', views.join, name='join'),
     path('student/', views.Student, name='student'),
 ]
