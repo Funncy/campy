ae1fb7d6cb636581215030909230a445ccecec58
funncy <gywns41@naver.com>
2019-06-17 17:59:14 +0900

    common model 수정 - subjectInfo 생성 - completionDivision - SubjectDomain

--- app_common_data/models.py
+++ app_common_data/models.py
@@ -5,9 +5,11 @@ from django.db import models
 class UniversityInfo(models.Model):
 
     # 대학 이름
+    # 최대학점 (4.5, 4.3, 4.0)
     university_name = models.CharField(max_length=20)
+    university_max_grade = models.FloatField()
 
-# 단과대학정보
+# 단과대학정보 (사용 x)
 class CollegeInfo(models.Model):
 
     # 대학 이름
@@ -23,24 +25,43 @@ class DepartmentInfo(models.Model):
     # 학과 이름
     university = models.ForeignKey(UniversityInfo, on_delete=models.CASCADE)
     university_name = models.CharField(max_length=20)
-    college_name = models.CharField(max_length=20)
+    college_name = models.CharField(max_length=20, blank=True)
     department_name = models.CharField(max_length=20)
 
 # 교과목정보
 class SubjectInfo(models.Model):
 
+    # 대학
     # 대학 이름
     # 교과목 코드
     # 교과목 이름
     # 교과목 이수구분
     # 교과목 영역
     # 교과목 학점
-    # 교과목 평가 방법
-
+    university = models.ForeignKey(UniversityInfo, on_delete=models.CASCADE)
     university_name = models.CharField(max_length=20)
     subject_code = models.CharField(max_length=20)
     subject_name = models.CharField(max_length=20)
-    subject_complete_division = models.CharField(max_length=20)
-    subject_area = models.CharField(max_length=20)  #수정
-    subject_credit = models.CharField(max_length=20)
-    subject_assessment_Methods = models.CharField(max_length=20)
+    subject_completion_division = models.CharField(max_length=20)
+    subject_area = models.CharField(max_length=20, blank=True)
+    subject_credit = models.FloatField()
+
+# 학교별 이수구분
+class CompletionDivision(models.Model):
+
+    # 대학
+    # 대학 이름
+    # 이수 구분 명
+    university = models.ForeignKey(UniversityInfo, on_delete=models.CASCADE)
+    university_name = models.CharField(max_length=20)
+    Division_name = models.CharField(max_length=20)
+
+# 학교별 과목영역
+class SubjectDomain(models.Model):
+
+    # 대햑
+    # 대학 이름
+    # 도메인 이름
+    university = models.ForeignKey(UniversityInfo, on_delete=models.CASCADE)
+    university_name = models.CharField(max_length=20)
+    Domain_name = models.CharField(max_length=20)
