66a7f11763882e1fed2ff8351157d1b70860927d
funncy <gywns41@naver.com>
2019-06-19 19:00:07 +0900

    studentInfo 복수/부 전공 저장

--- app_account_management/views.py
+++ app_account_management/views.py
@@ -17,7 +17,7 @@ def join(request):
         if request.user.is_staff is True:
             return redirect(reverse('index'))
         try:
-            student_info = StudentInfo.objects.get(user_id=request.user.id)
+            student_info = StudentInfo.objects.filter(user_id=request.user.id)
         except StudentInfo.DoesNotExist:
             return render(request, 'join.html', {})
 
@@ -28,12 +28,20 @@ def join(request):
 @login_required
 def Student(request):
     if request.method == 'POST':
-        #studentInfo 저장
-        #user = request.user
-        print('test')
-        #print(request.POST.get('university'))
-        print(request.user.username)
+        # 주전공 가져오기
         departmentInfo = DepartmentInfo.objects.get(pk=request.POST.get('department'))
+
+        # 복수/부 전공 가져오기
+        multiple_departmentInfo = None
+        sub_departments = None
+
+        if request.POST.get('multiple_department') != '0':
+            multiple_departmentInfo = DepartmentInfo.objects.get(pk=request.POST.get('multiple_department'))
+
+        if request.POST.get('sub_department') != '0':
+            sub_departments = DepartmentInfo.objects.get(pk=request.POST.get('sub_department'))
+
+        #학생정보 저장
         studentInfo = StudentInfo(user_id=request.user.id,
                                   student_admission_year=request.POST.get('admission_year'),
                                   student_university_name=departmentInfo.university_name,
@@ -43,5 +51,27 @@ def Student(request):
                                   student_college_name=departmentInfo.college_name
                                   )
         studentInfo.save()
+        #복수 전공 있을시 복수전공 저장
+        if multiple_departmentInfo is not None:
+            studentInfo = StudentInfo(user_id=request.user.id,
+                                      student_admission_year=request.POST.get('admission_year'),
+                                      student_university_name=multiple_departmentInfo.university_name,
+                                      student_major_division='복수전공',
+                                      student_name=request.user.username,
+                                      student_major_name=multiple_departmentInfo.department_name,
+                                      student_college_name=multiple_departmentInfo.college_name
+                                      )
+            studentInfo.save()
+        #부전공 있을시 부전공 저장
+        if sub_departments is not None:
+            studentInfo = StudentInfo(user_id=request.user.id,
+                                      student_admission_year=request.POST.get('admission_year'),
+                                      student_university_name=sub_departments.university_name,
+                                      student_major_division='부전공',
+                                      student_name=request.user.username,
+                                      student_major_name=sub_departments.department_name,
+                                      student_college_name=sub_departments.college_name
+                                      )
+            studentInfo.save()
         return HttpResponse()
     return HttpResponse()
