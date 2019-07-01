cddc59199c85f4d93491885c41b96e9cd018a930
funncy <gywns41@naver.com>
2019-07-01 15:29:50 +0900

    mypage 대학 변경시 학과 변경

--- app_common_data/urls.py
+++ app_common_data/urls.py
@@ -8,6 +8,7 @@ router.register(r'university/data', views.UniversityDataViewset)
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
-    path('departments/', views.set_deparment_and_college),
     path('', include(router.urls)),
+    path('departments/name/', views.set_deparment_and_college_by_name),
+    path('departments/', views.set_deparment_and_college_by_code),
 ]
--- app_common_data/views.py
+++ app_common_data/views.py
@@ -27,9 +27,41 @@ class UniversityDataViewset(viewsets.ReadOnlyModelViewSet):
         university = MetaDatainfo.objects.get(meta_data_relation_name=university, meta_data_code='meta_universityList')
         return MetaDatainfo.objects.filter(meta_data_code=self.request.query_params.get('meta_data_code'), upper_data_code=university.meta_data_relation_code)
 
+def set_deparment_and_college_by_name(request):
+    # request데이터 가져오기
+    # 상위대학 코드
+    meta_data_name = request.GET['meta_data_code']
+    university_code = MetaDatainfo.objects.get(meta_data_code='meta_universityList', meta_data_relation_name=meta_data_name)
+
+    colleges = MetaDatainfo.objects.filter(upper_data_code=university_code.meta_data_relation_code, meta_data_code='meta_collegeList')
+
+    colleges_and_departments = list()
+    i = 0
+    for college in colleges:
+        departments = MetaDatainfo.objects.filter(upper_data_code=college.meta_data_relation_code).values(
+            'meta_data_relation_name', 'meta_data_relation_code')
+        print(departments)
+        colleges_and_departments.append({'college_name': college.meta_data_relation_name})
+
+        colleges_and_departments[i]['departments'] = list()
+        department_count = 0
+        for department in departments:
+            print(department)
+            print(department['meta_data_relation_name'])
+            colleges_and_departments[i]['departments'].append({'department_code': department['meta_data_relation_code'],
+                                                               'department_name': department[
+                                                                   'meta_data_relation_name']})
+            department_count += 1
+            # colleges_and_departments[college.meta_data_relation_name].append(department['meta_data_relation_name'])
+        colleges_and_departments[i]['counts'] = department_count
+        i = i + 1
+
+    print(colleges_and_departments)
+
+    return JsonResponse(colleges_and_departments, safe=False)
 
 #리펙토링 필요
-def set_deparment_and_college(request):
+def set_deparment_and_college_by_code(request):
     #request데이터 가져오기
     # 상위대학 코드
     meta_data_code = request.GET['meta_data_code']
